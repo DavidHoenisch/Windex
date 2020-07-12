94c719a562bf5a8b64b41861c701792f72c53c76
David <davidjhoenisch@protonmail.com>
2020-07-11 18:12:06 -0600

    Update remove_bloat.ps1
    This update adds more functionality to the original script such as:
    * echo responses to current Fx in the script
    * outputs date that the scirpt was run to a log in C:\Logs (if this Dir does not exist, it creates it)
    * outputs the actual removal process to a log file for MicroWindex in the C:\Logs directory.

--- remove_bloat.ps1
+++ remove_bloat.ps1
@@ -1,7 +1,32 @@
+echo "Welcome to MicroWindex"
+
+if (Test-Path -Path C:\Users\squirrel\Documents)
+{"found CSV, proceeding to remove specified applications"}
+else
+{"No CSV file found, please add one to the Documents folder!"}
+
+Set-Location C:\
+if (Test-Path -Path C:\Logs)
+{"Found Log file Destination"}
+else
+{New-Item -Name "Logs" -ItemType "Directory"}
+
+echo "Removing Bloatware"
+
+echo "<===================================================>" >>  C:\Logs\MicroWindex.txt
+
+Get-Date >> C:\Logs\MicroWindex.txt
+
 $csv = Import-CSV -Path C:\Users\squirrel\Documents\microsoftbloat.csv
 
 foreach ($line in $csv) {
 	
-	Get-AppxPackage $line.appxpkg | Remove-AppxPackage -Verbose
+	Get-AppxPackage $line.appxpkg | Remove-AppxPackage -Verbose >> C:\Logs\MicroWindex.txt
 
 }
+
+echo "Bloatware Removed "
+Start-Sleep -Seconds 2
+echo "You can find a log of all the application that were removed in the C:\Logs directory" 
+Set-Location C:\Logs
+ls
